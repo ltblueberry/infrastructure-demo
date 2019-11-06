9b757bc7b0f3e1a24ef0667d78f0e0cedb92e79f
Andrey Romanov <aromanov.app@gmail.com>
2019-11-06 18:04:56 +0300

    added input variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,47 +1,51 @@
 provider "google" {
   version = "1.4.0"
-  project = "infra-258109"
-  region  = "europe-west4"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "dummy_app" {
-    name         = "dummy-app"
-    machine_type = "g1-small"
-    zone         = "europe-west4-b"
-    tags         = ["dummy-server"]
-    boot_disk {
-        initialize_params {
-            image = "nodejs-base"
-        }
-    }
-    network_interface {
-        network = "default"
-        access_config {}
-    }
-    metadata{
-        ssh-keys = "appuser:${file("~/.ssh/gcloud_rsa.pub")}"
-    }
+  name         = "dummy-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["${var.app_tag}"]
 
-    connection {
-        type        = "ssh"
-        user        = "appuser"
-        agent       = "false"
-        private_key = "${file("~/.ssh/gcloud_rsa")}"
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
     }
+  }
 
-    provisioner "remote-exec" {
-        script = "files/deploy.sh"
-    }
- }
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
 
- resource "google_compute_firewall" "firewall_app" {
-    name    = "allow-app-default"
-    network = "default"
-    allow {
-        protocol = "tcp"
-        ports    = ["3000"]
-    }
-    source_ranges = ["0.0.0.0/0"]
-    target_tags = ["dummy-server"]
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = "false"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
+resource "google_compute_firewall" "firewall_app" {
+  name    = "allow-app-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["${var.app_tag}"]
+}
