49014dd5688f4f8005e8fbb24c225c53a0215e43
Andrey Romanov <aromanov.app@gmail.com>
2019-11-08 11:39:39 +0300

    added terrafrom output var for db external ip

--- terraform-extended/modules/db/outputs.tf
+++ terraform-extended/modules/db/outputs.tf
@@ -1,3 +1,7 @@
+output "dummy_db_external_ip" {
+  value = "${google_compute_instance.dummy_db.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
 output "dummy_db_internal_ip" {
   value = "${google_compute_instance.dummy_db.network_interface.0.network_ip}"
 }
--- terraform-extended/prod/outputs.tf
+++ terraform-extended/prod/outputs.tf
@@ -2,6 +2,10 @@ output "dummy_app_external_ip" {
   value = "${module.app.dummy_app_external_ip}"
 }
 
+output "dummy_db_external_ip" {
+  value = "${module.db.dummy_db_external_ip}"
+}
+
 output "dummy_db_internal_ip" {
   value = "${module.db.dummy_db_internal_ip}"
 }
